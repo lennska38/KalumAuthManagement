81f54a28e0febf0a4c72dab6de06e7e402af634a
Kevin Ramos <lennykev38@gmail.com>
2023-08-16 17:44:21 -0600

    Todo el proyecto

---
+++ Migrations/20230811182212_identificationids.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KalumAuthManagement.Migrations
+{
+    public partial class identificationids : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
